299ed2897bbe1a2b44f70d44b9a4b3aff061efd4
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-18 15:54:04 +0200

    m

--- lvgl/demos/dental/app.c
+++ lvgl/demos/dental/app.c
@@ -37,7 +37,7 @@ extern lv_img_dsc_t topMidSelectionButtonImg;
 void reset_cb(lv_event_t* e) {
     lv_event_code_t code = lv_event_get_code(e);
     if (code == LV_EVENT_CLICKED) {
-        //printf("Click!!");
+        printf("Click!!");
         resetInitialChecked();
     }
 }
