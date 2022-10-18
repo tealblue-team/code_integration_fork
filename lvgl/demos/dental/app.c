20d01b7143ca3d0c7108b4348b53c0d1953742a0
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-18 15:55:23 +0200

    mo

--- lvgl/demos/dental/app.c
+++ lvgl/demos/dental/app.c
@@ -38,7 +38,7 @@ void reset_cb(lv_event_t* e) {
     lv_event_code_t code = lv_event_get_code(e);
     if (code == LV_EVENT_CLICKED) {
         printf("Click!!");
-        resetInitialChecked();
+        //resetInitialChecked();
     }
 }
 
