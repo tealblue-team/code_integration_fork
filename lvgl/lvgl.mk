79856e19da065c9b283fcab5ea725b2ddfd4c862
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-19 08:48:24 +0200

    m

--- lvgl/lvgl.mk
+++ lvgl/lvgl.mk
@@ -1,3 +1,4 @@
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/demos/dental/dental.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/core/lv_core.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw/lv_draw.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/extra/lv_extra.mk
@@ -5,4 +6,3 @@ include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/font/lv_font.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/misc/lv_misc.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/widgets/lv_widgets.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/demos/dental/dental.mk
