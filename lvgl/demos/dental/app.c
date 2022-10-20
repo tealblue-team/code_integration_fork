9a55c44436c6bd8a588b6ff99b9d621dba4b6f23
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-20 10:17:50 +0200

    M

--- lvgl/demos/dental/app.c
+++ lvgl/demos/dental/app.c
@@ -122,13 +122,13 @@ void app_create()
     lv_obj_t * rightLabel = lv_label_create(mainScreen);
     lv_obj_set_align(rightLabel, LV_ALIGN_LEFT_MID);
     lv_label_set_text(rightLabel, "R");
-    //lv_obj_set_size(rightLabel, 38, 750);
+    lv_obj_set_size(rightLabel, 16, 36);
     lv_obj_add_style(rightLabel, &textRobotoGreenStyle, LV_STATE_DEFAULT);
 
     lv_obj_t * leftLabel = lv_label_create(mainScreen);
     lv_obj_set_align(leftLabel, LV_ALIGN_RIGHT_MID);
     lv_label_set_text(leftLabel, "L");
-    //lv_obj_set_size(leftLabel, 750, 38);
+    lv_obj_set_size(rightLabel, 16, 36);
     lv_obj_add_style(leftLabel, &textRobotoGreenStyle, LV_STATE_DEFAULT);
 
     dentalArcInit(teethContainer); // Inizializzo bocca
