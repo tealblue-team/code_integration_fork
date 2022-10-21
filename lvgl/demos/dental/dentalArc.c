55d94bfd40f6047848d079538fd79693a35c3d0e
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-21 12:43:32 +0200

    m

--- lvgl/demos/dental/dentalArc.c
+++ lvgl/demos/dental/dentalArc.c
@@ -48,7 +48,7 @@ void dentalArcInit(lv_obj_t* parent) {
     if (!initialized) {
         /* init style pressed*/
         lv_color_t colorGreen = lv_color_hex(0x448300);
-        //lv_style_init(&greenPressedStyle);
+        lv_style_init(&greenPressedStyle);
         //lv_style_set_img_recolor_opa(&greenPressedStyle, LV_OPA_30);
         //lv_style_set_img_recolor(&greenPressedStyle, colorGreen);
 
