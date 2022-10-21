85c98abce670a0e95b4a6c0ec4d111c59cf1fb2d
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-21 10:26:02 +0200

    m

--- lvgl/demos/dental/selectButtons.c
+++ lvgl/demos/dental/selectButtons.c
@@ -90,9 +90,7 @@ void buttonsInit(lv_obj_t* parent) {
     lv_obj_add_event_cb(topSelectionButton, deselectAllOtherButtons, LV_EVENT_VALUE_CHANGED, NULL);
     lv_obj_add_event_cb(topSelectionButton, isOnBtnAreaCentered, LV_EVENT_ALL, NULL);
 
-    lv_coord_t botSelectBtnPos = lv_obj_get_width(parent) * 0.30;
-
-
+  
 
     BottomMidSelectionButton = lv_img_create(parent);
     imgButtonSetUp(BottomMidSelectionButton, &BottomMidSelectionButtonImg);
