f1243a8bd04f1b792880e2e8f287bde09e1a17c0
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-21 12:05:20 +0200

    m

--- lvgl/demos/dental/selectButtons.c
+++ lvgl/demos/dental/selectButtons.c
@@ -83,7 +83,7 @@ void buttonsInit(lv_obj_t* parent) {
     topSelectionButton = lv_img_create(parent);
     imgButtonSetUp(topSelectionButton, &topSelectionButtonImg);
     lv_obj_align(topSelectionButton, LV_ALIGN_CENTER,0,-120);
-    lv_obj_set_width(topSelectionButton, 50);
+    lv_obj_set_width(topSelectionButton, 200);
     lv_obj_add_flag(topSelectionButton, LV_OBJ_FLAG_CHECKABLE);
     lv_obj_clear_state(topSelectionButton, LV_STATE_CHECKED);
     lv_obj_add_style(topSelectionButton, &greenPressedStyle, LV_STATE_CHECKED);
